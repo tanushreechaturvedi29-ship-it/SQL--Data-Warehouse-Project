7d59379df36c107402febf456ce75a8a890083f8
tanushreechaturvedi29-ship-it <tanushreechaturvedi29@gmail.com>
2025-11-16 03:13:49 +0530

    Refactor database initialization script
    Dropped old databases and recreated DataWarehouse along with bronze, silver, and gold databases.

--- scripts/init_datbase.sql
+++ scripts/init_datbase.sql
@@ -13,67 +13,16 @@ WARNING:
     and ensure you have proper backups before running this script.
 
 */
-
--- Drop and recreate the 'DataWarehouse' database 
-
+-- Drop old databases if they exist
 DROP DATABASE IF EXISTS DataWarehouse;
+DROP DATABASE IF EXISTS bronze;
+DROP DATABASE IF EXISTS silver;
+DROP DATABASE IF EXISTS gold;
+
+-- Recreate databases
 CREATE DATABASE DataWarehouse;
-USE DataWarehouse;
+CREATE DATABASE bronze;
+CREATE DATABASE silver;
+CREATE DATABASE gold;
 
--- Create layer schemas (in MySQL = separate databases)
-CREATE DATABASE IF NOT EXISTS bronze;
-USE bronze;
-CREATE TABLE bronze.crm_cust_info (
-    cst_id INT,
-    cst_key NVARCHAR(50),
-    cst_firstname NVARCHAR(50),
-    cst_lastname NVARCHAR(50),
-    cst_material_status NVARCHAR(50),
-    cst_gndr NVARCHAR(50),
-    cst_create_date DATE
-);
-
-
-CREATE TABLE broneze.crm_prd_info(
-    prd_id INT,
-    prd_key NVARCHAR(50),
-    prd_nm NVARCHAR(50),
-    prd_cost INT,
-    prd_line  NVARCHAR(50),
-    prd_start_dt DATETIME,
-    prd_end_dt DATETIME
-);
-
-CREATE TABLE bronze.crm_sales_details (
-    sls_ord_num NVARCHAR(50),
-    sls_prd_key NVARCHAR(50),
-    sls_cust_id INT,
-    sls_order_dt INT,
-    sls_ship_dt INT,
-    sls_due_dt INT,
-    sls_sales INT,
-    sls_quantitt INT,
-    sls_price INT,
-);
-
-CREATE TABLE bronze.erp_loc_a101 (
-    cid NVARCHAR(50),
-    cntry NVARCHAR(50)
-);
-
-CREATE TABLE bronze.erp_cust_az12 (
-    cid NVARCHAR(50),
-    bdate DATE,
-    gen NVARCHAR(50)
-);
-
-CREATE TABLE bronze.erp_px_cat_g1v2 (
-    id NVARCHAR(50),
-    cat NVARCHAR(50),
-    subcat NVARCHAR(50),
-    maintenance NVARCHAR(50)
-);
-
-CREATE DATABASE IF NOT EXISTS silver;
-CREATE DATABASE IF NOT EXISTS gold;
 
